d562fb8b4ffbdb45f384d150a6941ac2b29efcdc
fallenangelX <iceis2@hotmail.com>
2011-08-12 05:02:53 -0400

    Rx2 :: 9 :: should add custom sd2 to compile under VC90

--- src/bindings/RxSd2/scripts/custom/custom_npcs.cpp
+++ src/bindings/RxSd2/scripts/custom/custom_npcs.cpp
@@ -34,7 +34,6 @@ custom_npc_teleportmaster
 custom_npc_titlemaster
 custom_npc_tokenvendor
 custom_script_training_dummy
-ViP_master
 */
 
 #include "precompiled.h"
@@ -6722,124 +6721,6 @@ CreatureAI* GetAI_custom_script_training_dummy(Creature* pCreature)
     return new custom_script_training_dummyAI(pCreature);
 }
 
-/*####
-# VIP_master
-####*/
-
-#define HOW_TO_BECOME_VIP    "How can I become a VIP?"
-#define QUESTLIST            "I would like to begin my quest VIP."
-#define VENDORLIST           "Show me your goods."
-#define NEWS                 "Tell me the news."
-#define STARTED_VIP_QUEST    "Thank you donated."
-#define TRAINLIST            "I would like to continue my education"
-
-struct MANGOS_DLL_DECL vip_masterAI : public ScriptedAI
-{
-    vip_masterAI(Creature *pCreature) : ScriptedAI(pCreature) {Reset();}
-
-    bool InCombat;
-
-    void Reset()
-    {
-        InCombat = false;
-    }
-
-    void AttackStart(Unit *pWho)
-    {
-        return; 
-    }
-
-    void Aggro(Unit *pWho)
-    {
-        return;
-    }
-
-};
-
-
-CreatureAI* GetAI_vip_master(Creature *pCreature)
-{
-    return new vip_masterAI (pCreature);
-}
-
-bool GossipHello_vip_master(Player *pPlayer, Creature *pCreature)
-{
-    ObjectGuid pguid = pPlayer->GetObjectGuid();
-    ObjectGuid cguid = pCreature->GetObjectGuid();
-
-    /*if (pCreature->isQuestGiver()) {
-        pPlayer->PrepareQuestMenu( cguid );
-        pPlayer->SendPreparedQuest( cguid );*/
-
-    if(pPlayer->isVIP( pguid ) == true)
-        {//sLog.outError("pPlayer VIP Status: %d", pPlayer->isVIP(pguid));
-        pPlayer->ADD_GOSSIP_ITEM( 2, QUESTLIST, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
-        pPlayer->ADD_GOSSIP_ITEM( 1, VENDORLIST, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_TRADE);
-        pPlayer->ADD_GOSSIP_ITEM( 3, TRAINLIST, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_TRAIN);}
-    else
-        {//sLog.outError("pPlayer VIP Status: %d", pPlayer->isVIP(pguid));
-        pPlayer->ADD_GOSSIP_ITEM( 2, HOW_TO_BECOME_VIP, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 2);}
-
-    pPlayer->ADD_GOSSIP_ITEM( 0, NEWS, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 4);
-    pPlayer->SEND_GOSSIP_MENU(DEFAULT_GOSSIP_MESSAGE, cguid);
-
-    return true;
-}
-
-bool GossipSelect_vip_master(Player *pPlayer, Creature *pCreature, uint32 sender, uint32 action)
-{
-    ObjectGuid pguid = pPlayer->GetObjectGuid();
-    ObjectGuid cguid = pCreature->GetObjectGuid();
-
-    if (action == GOSSIP_ACTION_INFO_DEF + 1) // Gib VIPQuest
-    {
-        /*QEmote qe;
-        qe._Delay = 0;
-        qe._Emote = 0;
-        std::string title = "";
-        pPlayer->pPlayerTalkClass->SendQuestGiverQuestList( qe, title, cguid ); */
-        if (pCreature->isQuestGiver()) {
-            pPlayer->PrepareQuestMenu( cguid );
-            pPlayer->SendPreparedQuest( cguid ); }
-    }
-    else
-        pPlayer->SEND_GOSSIP_MENU(110002,cguid); // VIPQuest fertig
-    
-    if (action == GOSSIP_ACTION_INFO_DEF + 2) // Wie werde ich VIP?
-        pPlayer->SEND_GOSSIP_MENU(110000,cguid);
-
-    if(action == GOSSIP_ACTION_TRADE)
-    {
-        pPlayer->SEND_VENDORLIST( pCreature->GetObjectGuid() );
-    }
-
-    if(action == GOSSIP_ACTION_TRAIN)
-    {
-        pPlayer->SEND_TRAINERLIST( pCreature->GetObjectGuid() );
-    }
-
-    if (action == GOSSIP_ACTION_INFO_DEF + 4) // News
-        pPlayer->SEND_GOSSIP_MENU(110001,cguid);
-
-    return true;
-}
-
-uint32 NPCDialogStatus_vip_master(Player *pPlayer, Creature *pCreature )
-{
-    return pPlayer->QUEST_DIALOG_STATUS(pPlayer, pCreature, DIALOG_STATUS_LOW_LEVEL_AVAILABLE);
-}
-
-bool QuestAccept_vip_master(Player *pPlayer, Creature *pCreaturereature, Quest const *quest)
-{
-    if(quest->GetQuestId() == 99000)
-    {
-        //((vip_masterAI*)creature->AI())->DoSay(STARTED_VIP_QUEST, LANG_UNIVERSAL, pPlayer);
-        return true;
-    }
-    return false;
-}
-
-
 void AddSC_custom_npcs()
 {
     Script *newscript;
@@ -6913,14 +6794,5 @@ void AddSC_custom_npcs()
     newscript = new Script;
     newscript->Name = "custom_script_training_dummy";
     newscript->GetAI = &GetAI_custom_script_training_dummy;
-    newscript->RegisterSelf(false);
-
-    newscript = new Script;
-    newscript->Name="vip_master";
-    newscript->pGossipHello          = &GossipHello_vip_master;
-    newscript->pGossipSelect         = &GossipSelect_vip_master;
-    newscript->pDialogStatusNPC      = &NPCDialogStatus_vip_master;
-    newscript->pQuestAcceptNPC       = &QuestAccept_vip_master;
-    newscript->GetAI = &GetAI_vip_master;
     newscript->RegisterSelf(false);
 }
